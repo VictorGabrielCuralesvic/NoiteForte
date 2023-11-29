af538bca6c9179a84a988f2585814614277480c1
Nome <miranhaomi@gmail.com>
2023-11-29 17:43:27 -0300

    Update news.js

--- server/src/news.js
+++ server/src/news.js
@@ -1,14 +1,13 @@
 const axios = require('axios');
 
-const newsType = 'br';
-
-const news = {
-    method: 'GET',
-    url: `https://fortnite-api.com/v2/news/${newsType}`
-}
-
-async function fetchData() {
+async function fetchData(newsType) {
     try {
+
+        const news = {
+            method: 'GET',
+            url: `https://fortnite-api.com/v2/news/${newsType}`
+        }
+        
         const response = await axios.request(news);
 
         console.log(JSON.stringify(response.data, null, 2));
@@ -17,4 +16,6 @@ async function fetchData() {
     }
 };
 
-fetchData();
+fetchData('br');
+fetchData('creative');
+fetchData('stw');
