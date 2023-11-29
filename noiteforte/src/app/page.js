7076e85968cb0d8d86c8bb43321f7dffb27bab77
elisonmartins <martinselison10@gmail.com>
2023-11-29 14:21:35 -0300

    Feat: criacao do frontend

---
+++ noiteforte/src/app/page.js
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-full items-center justify-between">
+        aa
+      </div>
+        
+    </main>
+  )
+}
