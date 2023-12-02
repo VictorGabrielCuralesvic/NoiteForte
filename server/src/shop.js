4b54f6c9a6fdc8bae99034d897fd7f8e5f21ad53
elisonmartins <martinselison10@gmail.com>
2023-12-02 18:17:38 -0300

    feat: Adicionando frontend com shop na dev

--- server/src/shop.js
+++ server/src/shop.js
@@ -1,3 +1,5 @@
+// shop.js
+
 const axios = require('axios');
 
 const brShop = {
@@ -8,39 +10,14 @@ const brShop = {
     }
 }
 
-async function fetchData() {
+async function fetchFortniteData() {
     try {
         const response = await axios.request(brShop);
-        
-        /* console.log(JSON.stringify(response.data, null, 2)); */
-        if (response.data && response.data.data && response.data.data.featured && response.data.data.featured.entries) {
-            // Itera sobre os itens e imprime várias informações de cada um
-            response.data.data.featured.entries.forEach(item => {
-                console.log(`Nome: ${item.items[0].name}`);
-                console.log(`Descrição: ${item.items[0].description}`);
-                console.log(`Tipo: ${item.items[0].type.displayValue}`);
-                console.log(`Raridade: ${item.items[0].rarity.displayValue}`);
-                
-                // Verifica se há informações sobre a série
-                if (item.items[0].series) {
-                    console.log(`Série: ${item.items[0].series.text}`);
-                } else {
-                    console.log('Série: N/A');
-                }
-
-                // Imprime URLs das imagens
-                console.log(`Ícone Pequeno: ${item.items[0].images.smallIcon}`);
-                console.log(`Ícone: ${item.items[0].images.icon}`);
-                console.log(`Imagem Destacada: ${item.items[0].images.featured}`);
-                
-                console.log('----------------------');
-            });
-        } else {
-            console.log('Nenhum item encontrado.');
-        }
+        return response.data.data.featured.entries;
     } catch (error) {
         console.log(error);
+        return null;
     }
-};
+}
 
-fetchData();
+export default fetchFortniteData;
---
+++ web/noiteforte/src/app/components/FortniteItem.js
@@ -0,0 +1,99 @@
+// components/FortniteItem.js
+import { useEffect, useState } from 'react';
+import fetchData from '../../../../../server/src/shop.js';
+
+const FortniteItem = ({ searchTerm }) => {
+  const [items, setItems] = useState([]);
+  const [currentPackageIndex, setCurrentPackageIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const data = await fetchData();
+      if (data) {
+        setItems(data);
+      }
+    };
+
+    fetchItems();
+  }, [searchTerm]);
+
+  const showNextPackage = () => {
+    setCurrentPackageIndex((prevIndex) => (prevIndex + 1) % items.length);
+  };
+
+  return (
+    <div>
+      {items.length > 0 && (
+        <div className="border p-4 mb-4 rounded-md bg-[#08BDE6]">
+          {/* Renderizar os detalhes do item */}
+          {items[currentPackageIndex].bundle && items[currentPackageIndex].bundle.name && (
+            <h2 className="text-xl font-bold mb-2 text-fortnite-text">
+              {items[currentPackageIndex].bundle.name}
+            </h2>
+          )}
+          {items[currentPackageIndex].bundle && items[currentPackageIndex].bundle.info && (
+            <p className="text-gray-700">{items[currentPackageIndex].bundle.info}</p>
+          )}
+          {items[currentPackageIndex].regularPrice && (
+            <p className="text-fortnite-text">
+              Preço Regular: {items[currentPackageIndex].regularPrice} V-Bucks
+            </p>
+          )}
+          {items[currentPackageIndex].finalPrice && (
+            <p className="text-fortnite-text">
+              Preço Final: {items[currentPackageIndex].finalPrice} V-Bucks
+            </p>
+          )}
+          {items[currentPackageIndex].banner && items[currentPackageIndex].banner.value && (
+            <p className={`text-fortnite-${items[currentPackageIndex].banner.value.toLowerCase()}`}>
+              Desconto: {items[currentPackageIndex].banner.value}
+            </p>
+          )}
+          {items[currentPackageIndex].giftable && (
+            <p>Giftable: {items[currentPackageIndex].giftable ? 'Sim' : 'Não'}</p>
+          )}
+          {items[currentPackageIndex].refundable && (
+            <p>Refundable: {items[currentPackageIndex].refundable ? 'Sim' : 'Não'}</p>
+          )}
+
+          {/* Renderizar os itens do bundle */}
+          {items[currentPackageIndex].items &&
+            items[currentPackageIndex].items.map((bundleItem, bundleIndex) => (
+              <div key={bundleIndex} className="border p-4 mb-4 rounded-md bg-[#cecece]">
+                <h3 className="text-lg font-bold mb-2 text-fortnite-text">
+                  {bundleItem.name}
+                </h3>
+                {bundleItem.description && (
+                  <p className="text-gray-700">{bundleItem.description}</p>
+                )}
+                {bundleItem.type && bundleItem.type.displayValue && (
+                  <p className="text-fortnite-text">
+                    Tipo: {bundleItem.type.displayValue}
+                  </p>
+                )}
+                {bundleItem.rarity && bundleItem.rarity.displayValue && (
+                  <p className={`text-fortnite-${bundleItem.rarity.displayValue.toLowerCase()}`}>
+                    Raridade: {bundleItem.rarity.displayValue}
+                  </p>
+                )}
+                {bundleItem.series && bundleItem.series.text && (
+                  <p>Série: {bundleItem.series.text}</p>
+                )}
+                {bundleItem.images && bundleItem.images.icon && (
+                  <img src={bundleItem.images.icon} alt="Ícone" className="my-2 mx-auto" />
+                )}
+              </div>
+            ))}
+        </div>
+      )}
+
+      {items.length > 1 && (
+        <button onClick={showNextPackage} className="bg-blue-500 text-white px-4 py-2 rounded">
+          Próximo Pacote
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default FortniteItem;
